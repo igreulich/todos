8b835be795baed12e2c196f06082ca0a71c5112b
Ian Greulich <ian@greulich.me>
2019-08-23 15:12:36 -0400

    basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const webpack = require('webpack');
+const flowright = require('lodash.flowright');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV || '';
+
+function generateBaseConfig() {
+  const config = {
+    mode: '',
+    devtool: '',
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+    },
+    module: {},
+    plugins: [],
+  }
+
+  return config
+}
+
+function applyEnv(config) {
+  let newConfig;
+
+  if (nodeEnv === 'production') {
+    newConfig = {
+      ...config,
+      mode: 'production',
+      devtool: 'source-map',
+      optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              chunks: 'initial',
+              enforce: true,
+            },
+          },
+        },
+        minimizer: [
+          new TerserPlugin({
+            sourceMap: true,
+            parallel: true,
+          }),
+        ],
+      },
+    };
+  } else {
+    newConfig = {
+      ...config,
+      mode: 'development',
+      devtool: 'cheap-module-eval-source-map',
+      devServer: {
+        port: 3000,
+        historyApiFallback: true,
+        hot: true,
+        open: true,
+        overlay: true,
+      },
+    };
+  }
+
+  return newConfig;
+}
+
+function applyLoaders(config) {
+  let newConfig;
+
+  newConfig = {
+    ...config,
+    module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [],
+        }
+      }],
+    },
+  };
+
+  return newConfig;
+}
+
+const configurate = flowright(
+  applyLoaders,
+  applyEnv,
+  generateBaseConfig,
+);
+
+module.exports = configurate();
