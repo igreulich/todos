378a47f88a96e88cec2045bda820fed2a9c2d0c7
Ian Greulich <ian@greulich.me>
2019-08-28 15:38:47 -0400

    update header

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import { Header, Segment } from 'semantic-ui-react';
 
+import './Header.less';
+
 const Head = () => (
-  <Segment as="header" basic>
-    <Header icon="plug" content="Uptime Guarantee" />
+  <Segment as="header" basic clearing inverted>
+    <Header className="todo-list__header" content="Over Engineered Todos" icon="list alternate outline" floated="left" />
+    <Header className="todo-list__header" icon="plus" floated="right" />
   </Segment>
 );
 
