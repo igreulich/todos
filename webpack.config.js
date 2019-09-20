2af7a47ef879d12aeefe8876843e0874ae44ad2d
Ian Greulich <ian@greulich.me>
2019-09-20 17:01:31 -0400

    fix style extract plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
 const path = require('path');
-const flowright = require('lodash.flowright');
+const flow = require('lodash.flow');
 const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const NODE_ENV = process.env.NODE_ENV || '';
+const MODES = {
+  PRODUCTION: 'production',
+  DEVELOPMENT: 'development',
+  TEST: 'test',
+};
+const env = process.env.NODE_ENV || MODES.DEVELOPMENT;
 
 function generateBaseConfig() {
-  const config = {
+  let devtool;
+  let mode;
+
+  if (env === MODES.PRODUCTION) {
+    devtool = 'source-map';
+    mode = MODES.PRODUCTION;
+  } else {
+    devtool = 'cheap-module-eval-source-map';
+    mode = MODES.DEVELOPMENT;
+  }
+
+  return {
     context: path.resolve(__dirname),
     entry: {
       app: ['react-hot-loader/patch', './src/index.js'],
@@ -17,74 +33,27 @@ function generateBaseConfig() {
       filename: '[name].js',
       publicPath: '/',
     },
-    mode: '',
-    devtool: '',
-    resolve: {
-      extensions: ['*', '.js', '.jsx'],
-      alias: {
-        'react-dom': '@hot-loader/react-dom',
-      },
-    },
-    module: {},
     plugins: [
       new HtmlWebpackPlugin({
         template: './src/index.html',
         title: 'Over Engineered Todo List App',
       }),
     ],
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+      alias: {
+        'react-dom': '@hot-loader/react-dom',
+      },
+    },
+    devtool,
+    mode,
   };
-
-  return config;
-}
-
-function applyEnv(config) {
-  let newConfig;
-
-  if (NODE_ENV === 'production') {
-    newConfig = {
-      ...config,
-      mode: 'production',
-      devtool: 'source-map',
-      optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-          cacheGroups: {
-            vendor: {
-              test: /[\\/]node_modules[\\/]/,
-              chunks: 'initial',
-              enforce: true,
-            },
-          },
-        },
-        minimizer: [
-          new TerserPlugin({
-            sourceMap: true,
-            parallel: true,
-          }),
-        ],
-      },
-    };
-  } else {
-    newConfig = {
-      ...config,
-      mode: 'development',
-      devtool: 'cheap-module-eval-source-map',
-      devServer: {
-        port: 3000,
-        historyApiFallback: true,
-        hot: true,
-        open: true,
-        overlay: true,
-      },
-    };
-  }
-
-  return newConfig;
 }
 
 function applyLoaders(config) {
-  const devMode = NODE_ENV !== 'production';
-  const newConfig = {
+  const styleLoader = (env === MODES.PRODUCTION) ? MiniCssExtractPlugin.loader : 'style=loader';
+
+  return {
     ...config,
     module: {
       rules: [{
@@ -93,9 +62,7 @@ function applyLoaders(config) {
         loader: ['babel-loader', 'eslint-loader'],
       }, {
         test: /\.less$/,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'less-loader',
-        ],
+        use: [styleLoader, 'css-loader', 'less-loader'],
       }, {
         test: /\.(woff(2)?|ttf|eot|svg|png)(\?v=\d+\.\d+\.\d+)?$/,
         use: [{
@@ -107,14 +74,80 @@ function applyLoaders(config) {
       }],
     },
   };
-
-  return newConfig;
 }
 
-const configurate = flowright(
-  applyLoaders,
-  applyEnv,
+function applyPlugins(config) {
+  let { plugins } = config;
+
+  if (env === MODES.PRODUCTION) {
+    plugins = [
+      ...plugins,
+      new MiniCssExtractPlugin({
+        filename: 'public/styles/[name].css',
+      }),
+    ];
+  } else {
+    plugins = [
+      ...plugins,
+    ];
+  }
+
+  return {
+    ...config,
+    plugins,
+  };
+}
+
+function applyOptimizations(config) {
+  const optimization = {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'initial',
+          enforce: true,
+        },
+      },
+    },
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true,
+        parallel: true,
+      }),
+    ],
+  };
+
+  return {
+    ...config,
+    optimization: env === MODES.PRODUCTION ? optimization : null,
+  };
+}
+
+function applyServer(config) {
+  const devServer = {
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+    overlay: true,
+  };
+
+  return {
+    ...config,
+    devServer: (env === MODES.DEVELOPMENT) ? devServer : null,
+  };
+}
+
+const configurate = flow(
   generateBaseConfig,
+  applyLoaders,
+  applyPlugins,
+  applyOptimizations,
+  applyServer,
 );
 
-module.exports = configurate();
+module.exports = Object.entries(configurate()).reduce(
+  (a, [k, v]) => (v == null ? a : { ...a, [k]: v }),
+  {},
+);
