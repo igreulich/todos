07056479d5d3df98c52ec384197bd3f531dd14a6
Ian Greulich <ian@greulich.me>
2019-10-01 16:55:13 -0400

    convert generic async actions to something more inline with reality

--- src/features/todos/todosReducer.js
+++ src/features/todos/todosReducer.js
@@ -3,6 +3,14 @@ import { createSlice } from 'redux-starter-kit';
 import uuid from 'uuid';
 import ls from 'local-storage';
 
+// This is not a typical function you will need to make. I had to make a couple
+// of consessions because I don't have an api with a database to use.
+const fakeFetch = (value) => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(value);
+  }, 500);
+});
+
 const todosSlice = createSlice({
   slice: 'todos',
   initialState: { todos: [] },
@@ -48,32 +56,39 @@ const {
   toggleTodo,
 } = actions;
 
+// I did take some liberties in creating ids, deleting todos, and with storage.
+// There is some work done in this file that would be done by a database, or on
+// the other side of an api call.
+//
+// But generally this is how things work.
 export const createTodo = (payload) => (dispatch) => {
   const todo = { ...payload, id: uuid() };
 
-  setTimeout(() => {
-    dispatch(addTodo(todo));
-  }, 500);
+  // In a real app, all of this would be a `fetch()` call, if not a library like axios.
+  // It would *not* be a promise you made.
+  fakeFetch(todo).then((response) => {
+    dispatch(addTodo(response));
+  });
 };
 
 export const deleteTodo = (payload) => (dispatch) => {
-  setTimeout(() => {
+  fakeFetch(true).then(() => {
     dispatch(removeTodo(payload));
-  }, 500);
+  });
 };
 
 export const fetchTodos = () => (dispatch) => {
   const todos = ls('todos');
 
-  setTimeout(() => {
+  fakeFetch(todos).then(() => {
     dispatch(setTodos(todos));
-  }, 500);
+  });
 };
 
 export const toggleTodoDone = (payload) => (dispatch) => {
-  setTimeout(() => {
+  fakeFetch(true).then(() => {
     dispatch(toggleTodo(payload));
-  }, 500);
+  });
 };
 
 export default reducer;
